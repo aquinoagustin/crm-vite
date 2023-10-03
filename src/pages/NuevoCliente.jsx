c7842f69a4c7035eac191e88cb5edddab0aff7f3
aquinoagustin <aagustin067@gmail.com>
2023-10-03 08:48:53 -0300

    Se importaron los componentes Formulario y Forms

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Form } from "react-router-dom";
+import Formulario from '../components/Formulario';
+
+export function action(){
+  console.log('Submit al formulario...')
+  return 0;
+}
 
 export default function NuevoCliente() {
   const navigate = useNavigate();
@@ -16,7 +22,14 @@ export default function NuevoCliente() {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        Formulario Aqui
+        <Form method="post" >
+        <Formulario/>
+        <input
+          type="submit"
+          value='Registrar Cliente'
+          className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg mt-20"
+        />
+        </Form>
       </div>
     </>
   );
