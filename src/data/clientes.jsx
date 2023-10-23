c72f2198d38c29e72d3086596e86f4ca9f1fb665
Agus2020 <aagustin067@gmail.com>
2023-10-23 18:05:43 -0300

    Agregada funcion para editar y eliminar

--- src/data/clientes.jsx
+++ src/data/clientes.jsx
@@ -3,6 +3,13 @@ export async function obtenerClientes(){
      const resultado = await respuesta.json()
      return resultado;
 }
+
+export async function obtenerCliente(id){
+     const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`)
+     const resultado = await respuesta.json()
+     return resultado
+}
+
 export async function agregarCliente(datos){
      try{
           const respuesta = await fetch(import.meta.env.VITE_API_URL,{
@@ -11,6 +18,31 @@ export async function agregarCliente(datos){
                headers:{
                     'Content-Type':'application/json'
                }
+          })
+          await respuesta.json()
+     }catch(error){
+          console.log(error)
+     }
+}
+export async function actualizarCliente(id,datos){
+     try{
+          const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`,{
+               method:'PUT',
+               body:JSON.stringify(datos),
+               headers:{
+                    'Content-Type':'application/json'
+               }
+          })
+          await respuesta.json()
+     }catch(error){
+          console.log(error)
+     }
+}
+
+export async function eliminarCliente(id){
+     try{
+          const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`,{
+               method:'DELETE'
           })
           await respuesta.json()
      }catch(error){
