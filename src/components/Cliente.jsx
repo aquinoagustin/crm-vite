f216d789c0e356fe3f6d0debf4ba2ea36a91fcfe
Agus2020 <aagustin067@gmail.com>
2023-10-23 18:04:04 -0300

    Consumiendo API

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,4 +1,11 @@
+import { useNavigate,Form,redirect } from "react-router-dom";
+import { eliminarCliente } from "../data/clientes";
+export async function action({params}){
+  await eliminarCliente(params.clienteId)
+  return redirect('/')
+}
 export default function Cliente({ cliente }) {
+  const navigate = useNavigate();
   const { nombre, telefono, empresa, email, id } = cliente;
   return (
     <>
@@ -20,17 +27,25 @@ export default function Cliente({ cliente }) {
             <span className="text-gray-800 uppercase font-bold">
               Telefono:
             </span>{" "}
+            {telefono}{" "}
           </p>
         </td>
         <td className="p-6 flex gap-3">
           <button
             type="button"
             className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+            onClick={()=>navigate(`/clientes/${id}/editar`)}
           >Editar</button>
-          <button
-            type="button"
-            className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
-          >Editar</button>
+          <Form method="post" action={`/clientes/${id}/eliminar`} onSubmit={(e)=>{
+            if(!confirm('Esta seguro que desea eliminar este registro')){
+              e.preventDefault()
+            }
+          }}>
+            <button
+              type="submit"
+              className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+            >Eliminar</button>
+          </Form>
         </td>
       </tr>
     </>
